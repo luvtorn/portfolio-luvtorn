548c18533c7d52851c2f87f4d02bb20d4bce7da3
luvtorn <themrkolya0123@gmail.com>
2026-03-19 00:10:27 +0100

    Updates

--- _next/static/z-QkJzkjdBzjsDGINHT4M/_buildManifest.js
+++ _next/static/z-QkJzkjdBzjsDGINHT4M/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/portfolio-luvtorn//_next/:path+",
-        "destination": "/portfolio-luvtorn/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
