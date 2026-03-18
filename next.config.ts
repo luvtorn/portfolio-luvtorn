211d57439587b69a70b8215e3f7477798a8782d8
luvtorn <themrkolya0123@gmail.com>
2026-03-19 00:21:15 +0100

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: '/portfolio-luvtorn',
-  trailingSlash: true,
   reactCompiler: true,
   images: {
     unoptimized: true,
